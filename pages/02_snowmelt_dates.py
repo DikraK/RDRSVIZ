9b8e4af7b2bf52f1342f7a9981299414668db0db
Dikra Khedhaouiria <dikraa.khedhaouiria@ec.gc.ca>
2024-02-23 14:31:04 +0000

    adjust figsize

--- pages/02_snowmelt_dates.py
+++ pages/02_snowmelt_dates.py
@@ -239,7 +239,7 @@ data_prcp_agg_v01v3  = data_prcp_agg_v01v3_[data_prcp_agg_v01v3_['year'] == 1992
 
 unit = 1000
 
-fig = plt.figure(figsize=(4, 2))
+fig = plt.figure(figsize=(5.2, 2.6))
 
 ax = fig.add_subplot(111)
 
